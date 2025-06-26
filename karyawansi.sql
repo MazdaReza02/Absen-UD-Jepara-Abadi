692b63eaff4708c814043d62439d52b1976ae13a
Nama Baru <azharimazdareza@gmail.com>
2025-06-26 13:27:43 +0700

    Create karyawansi.sql

---
+++ karyawansi.sql
@@ -0,0 +1,185 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 05, 2023 at 03:22 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `karyawansi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_absen`
+--
+
+CREATE TABLE `tb_absen` (
+  `id` int(11) NOT NULL,
+  `id_karyawan` varchar(255) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `waktu` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_daftar`
+--
+
+CREATE TABLE `tb_daftar` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_daftar`
+--
+
+INSERT INTO `tb_daftar` (`id`, `username`, `password`) VALUES
+(21202, 'Mazda', '021202'),
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_jabatan`
+--
+
+CREATE TABLE `tb_jabatan` (
+  `id` int(11) NOT NULL,
+  `jabatan` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_jabatan`
+--
+
+INSERT INTO `tb_jabatan` (`id`, `jabatan`) VALUES
+(1, 'CEO'),
+(2, 'Admin Marketing'),
+(3, 'Admin Keuangan'),
+(4, 'Manager Oprasional'),
+(5, 'Finishing'),
+(6, 'Carpenter/Tukang Kayu');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_karyawan`
+--
+
+CREATE TABLE `tb_karyawan` (
+  `id_karyawan` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `tmp_tgl_lahir` varchar(255) NOT NULL,
+  `jenkel` varchar(255) NOT NULL,
+  `agama` varchar(255) NOT NULL,
+  `alamat` text NOT NULL,
+  `no_tel` varchar(18) NOT NULL,
+  `jabatan` varchar(255) NOT NULL,
+  `foto` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_keterangan`
+--
+
+CREATE TABLE `tb_keterangan` (
+  `id` int(11) NOT NULL,
+  `id_karyawan` varchar(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `keterangan` varchar(255) NOT NULL,
+  `alasan` text NOT NULL,
+  `waktu` datetime NOT NULL,
+  `bukti` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_absen`
+--
+ALTER TABLE `tb_absen`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tb_daftar`
+--
+ALTER TABLE `tb_daftar`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tb_jabatan`
+--
+ALTER TABLE `tb_jabatan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tb_karyawan`
+--
+ALTER TABLE `tb_karyawan`
+  ADD PRIMARY KEY (`id_karyawan`);
+
+--
+-- Indexes for table `tb_keterangan`
+--
+ALTER TABLE `tb_keterangan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_absen`
+--
+ALTER TABLE `tb_absen`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT for table `tb_daftar`
+--
+ALTER TABLE `tb_daftar`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `tb_jabatan`
+--
+ALTER TABLE `tb_jabatan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `tb_keterangan`
+--
+ALTER TABLE `tb_keterangan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
