75eefcf733cd3111a51df3f286fe061a0a77e9fa
Nama Baru <azharimazdareza@gmail.com>
2025-06-26 13:30:48 +0700

    Create registrasi.php

---
+++ registrasi.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Registrasi UD Jepara Abadi</title>
+<link rel="icon" href="assets/img/Logo-DEVanoda.png" type="image/png">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf-8" />
+<script type="application/x-javascript"> 
+  addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); 
+  function hideURLbar(){ window.scrollTo(0,1); } 
+</script>
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="//fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,700,700i" rel="stylesheet">
+</head>
+<body>
+  <div class="main-w3layouts wrapper">
+    <center><img src="assets/img/Logo-DEVanoda.png" width="250" class="img img-responsive" height="90"></center>
+    <h1><b>Registrasi UD Jepara Abadi</b></h1>
+    <div class="main-agileinfo">
+      <div class="agileits-top">
+        <form action="proses_registrasi.php" method="post">
+          <input class="text" type="text" name="username" placeholder="Username" required=""><br>
+          <input class="text" type="password" name="password" placeholder="Password" required=""><br>
+          <input class="text" type="password" name="konfirmasi_password" placeholder="Konfirmasi Password" required="">
+          <input type="submit" value="Daftar">
+        </form>
+        <p>Sudah punya akun? <a href="login.php">Login di sini</a></p>
+      </div>
+    </div>
+    <div class="colorlibcopy-agile">
+      <p>© 2022  UD Jepara Abadi  All rights reserved</p>
+    </div>
+    <ul class="colorlib-bubbles">
+      <li></li><li></li><li></li><li></li><li></li>
+      <li></li><li></li><li></li><li></li><li></li>
+    </ul>
+  </div>
+</body>
+</html>
